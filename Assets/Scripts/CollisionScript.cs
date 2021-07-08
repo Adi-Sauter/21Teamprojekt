b3076a985edf3fe20578721a7611a11ff09e8571
Adi-Sauter <adriansauter07.As@gmail.com>
2021-07-08 12:21:41 +0200

    Update

--- Assets/Scripts/CollisionScript.cs
+++ Assets/Scripts/CollisionScript.cs
@@ -14,29 +14,11 @@ public class CollisionScript : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         StartCoroutine(Teleportation(other));
-        
-        //Debug.Log("Collision with " + other.gameObject.name);
-        // if(other.gameObject.CompareTag("toSimulator"))
-        // {
-        //     //Debug.Log("if-case: Collision with " + other.gameObject.name);
-        //     FadingOut();
-        //     StartCoroutine(Wait());
-        //     this.transform.localPosition = new Vector3(targetinSim.transform.position.x, this.transform.position.y, targetinSim.transform.position.z);
-        // } else if(other.gameObject.CompareTag("toMuseum"))
-        // {
-        //     //Debug.Log("elseif-case: Collision with " + other.gameObject.name);
-        //     FadingOut();
-        //     StartCoroutine(Wait());
-        //     this.transform.localPosition = new Vector3(targetinMus.transform.position.x, this.transform.position.y, targetinMus.transform.position.z);
-        // }
     }
      public void FadingOut()
     {
         FadeScreen.GetComponent<Animation>().Play("BlackFade");
     }
-
-
-
     IEnumerator Teleportation(Collider other) 
     {
         if(other.gameObject.CompareTag("toSimulator"))
