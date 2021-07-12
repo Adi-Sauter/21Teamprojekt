93723ddc5a5c5bceb27ab8caede6dff9bf5b09ed
REEB2015 <85887862+REEB2015@users.noreply.github.com>
2021-07-12 18:44:54 +0100

    Automatische Graphenauswahl

--- Assets/ZFBrowser/Scripts/BrowserUI/ExtractImage.cs
+++ Assets/ZFBrowser/Scripts/BrowserUI/ExtractImage.cs
@@ -13,33 +13,40 @@ public class ExtractImage : MonoBehaviour
 
     public Browser DisplayCenter;
 
-    
-    /*
-    void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.T))
-        {
-            StartCoroutine(this.fetchTemperaturePrediction());
-        }
-        else if (Input.GetKeyUp(KeyCode.R))
-        {
-            StartCoroutine(this.fetchRightGraph(1));
-        }
-        else if (Input.GetKeyUp(KeyCode.L))
-        {
-            StartCoroutine(this.fetchLeftGraph(0));
-        }
+    public PointerUIBase Proxy;
+
+    void Start() {
+        StartCoroutine(this.GetTheRightGraph());
     }
-    */
+    
+    public IEnumerator GetTheRightGraph() {
+        
+        yield return new WaitForSeconds(10f);
+        this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.INITIAL;
+        Debug.Log("Get The Right Graph Now!!");
+        yield return new WaitForSeconds(2f);
+        this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.LeftGraph1;
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Nocheinen ");
+        this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.LeftGraph2;
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Und nochmal");
+        this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.LeftGraph3;
+        yield return new WaitForSeconds(2f);
+        this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.RightGraph1;
+        yield return new WaitForSeconds(2f);
+        this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.RightGraph2;
+        yield return new WaitForSeconds(2f);
+        Debug.Log("Der letzte!");
+        this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.RightGraph3;
+
+    }
     
     public IEnumerator fetchTemperaturePrediction()
     {
-        var promise = this.browser.EvalJS("document.getElementsByClassName(\"primary-temp-value svelte-1o6bcxn\")[0].innerHTML");
+        var promise = this.browser.EvalJS("document.getElementsByClassName(\"primary-temp-value svelte-1xplu3t\")[0].innerHTML");
         yield return promise.ToWaitFor();
         Debug.Log("promised value: " + promise.Value);
-        this.DisplayCenter.EvalJS("document.getElementByID(\"image-container\").src = " + promise.Value + ";");
-        this.DisplayCenter.EvalJS("document.getElementById(\"image-container\").width = '" + 500 + "';");
-        this.DisplayCenter.EvalJS("document.getElementById(\"image-container\").height = '" + 500 + "';");
     }
     
 
