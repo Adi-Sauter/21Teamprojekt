47c1e2975e0ccce3a56e58377530bd72f607e9fc
Adi-Sauter <adriansauter07.As@gmail.com>
2021-07-19 14:48:48 +0200

    Update

--- Assets/ZFBrowser/Scripts/BrowserUI/SetSliderToOriginal.cs
+++ Assets/ZFBrowser/Scripts/BrowserUI/SetSliderToOriginal.cs
@@ -2,41 +2,134 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ZenFulcrum.EmbeddedBrowser;
+using UnityEngine.UI;
 
-public class SetSliderToOriginal : MonoBehaviour
+public class RaycastScript : MonoBehaviour
 {
 
     public PointerUIBase Proxy;
 
-    // Start is called before the first frame update
-    void Test()
+    private GameObject hitObject;
+
+    public ExtractImage Image;
+    void Update()
     {
-        StartCoroutine(this.ResetSlider());
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.transform)
+                {
+                    //PrintName(hit.transform.gameObject);
+                    this.hitObject = hit.transform.gameObject;
+                    CurrentClickedGameObject(hit.transform.gameObject);
+                }   
+            }
+        }
+        if(Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                // perform a specified action on the object that was hit last
+                CurrentReleasedObject(hitObject);
+
+                // different method, where it´s checked first wether hit.transform exists (hit.transform != null)
+                // if(hit.transform)
+                // {
+                //      if(hit.transform.gameObject.tag != hitObject.tag)
+                //     {
+                //         Debug.Log("hit.transform != hitObject");
+                //         PrintTag(hit.transform.gameObject.tag, "new hitObject");
+                //         PrintTag(hitObject.tag, "hitobject");
+                //         CurrentReleasedObject(hitObject);
+                //     } else
+                //     {
+                //         Debug.Log("hit.transform == hitObject");
+                //         PrintTag(hit.transform.gameObject.tag, "new hitObject");
+                //         PrintTag(hitObject.tag, "hitobject");
+                //         CurrentReleasedObject(hit.transform.gameObject);
+                //     }
+                // }
+            }
+        }
     }
+    private void CurrentClickedGameObject(GameObject go)
+    {
+        if(go.tag == "Handle")
+        {
+            go.GetComponent<Image>().color = Color.white;
+            go.GetComponentInChildren<MeshRenderer>().material.color = Color.white;
+        }
+        if(go.tag == "Button")
+        {
+            go.GetComponent<Image>().color = Color.black;
+            go.GetComponentInChildren<MeshRenderer>().material.color = Color.black;
+            StartCoroutine(ResetSlider());
+           
+        }
+    }
+    private void CurrentReleasedObject(GameObject go)
+    {
+        if(go.tag == "Handle")
+        {
+        go.GetComponent<Image>().color = Color.red;
+        go.GetComponentInChildren<MeshRenderer>().material.color = Color.red; 
+        }
+        if(go.tag == "Button")
+        {
+            go.GetComponent<Image>().color = Color.blue;
+            go.GetComponentInChildren<MeshRenderer>().material.color = Color.blue;
+        }
+    }
+    
+    private void PrintName(GameObject go)
+    {
+        Debug.Log(go.name);
+    }
+    private void PrintTag(string tag, string name)
+    {
+        Debug.Log("Name des " + name + ": " + tag);
+    }
+
+    // void Test()
+    // {
+    //     StartCoroutine(this.ResetSlider());
+    // }
 
     public IEnumerator ResetSlider() {
         
+        yield return new WaitForSeconds(0.5f);
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.TRANSPORT_ELEC;
         Debug.Log("Reset first Slider");
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.TRANSPORT_EFFIC;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         Debug.Log("Noch einen ");
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.ENERGY_COAL;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         Debug.Log("Und nochmal");
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.LAND_METHANE;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.ENERGY_RENEWABLES;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.ENERGY_CARBON;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.BUILDING_ENERGY;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.GROWTH_ECONOMIC;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         Debug.Log("Der letzte!");
         this.Proxy.relevantProxyType = MouseClickRobot.PROXY_TYPE.GROWTH_POPULATION;
+        yield return new WaitForSeconds(0.5f);
+        StartCoroutine(Image.fetchTemperaturePrediction());
+        StartCoroutine(Image.fetchRightGraph(1));
+        StartCoroutine(Image.fetchLeftGraph(0));
 
     }
 
